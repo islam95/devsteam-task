9099ebe0d0ee55be5aef942bba2024c71328d35b
Islam Dudaev <islamwolf23@gmail.com>
2019-08-23 16:59:05 +0100

    Create action and types for redux

---
+++ redux/actions/cadastralAction.js
@@ -0,0 +1,20 @@
+import * as Types from "./types";
+import * as api from "../../helpers/api";
+
+export const fetchData = () => {
+  return async dispatch => {
+    dispatch({ type: Types.FETCH_DATA });
+    try {
+      const { data } = await api.getRegions();
+      dispatch({
+        type: Types.FETCH_DATA_SUCCESS,
+        data
+      });
+    } catch (err) {
+      dispatch({
+        type: Types.FETCH_DATA_ERROR,
+        error: err
+      });
+    }
+  };
+};
